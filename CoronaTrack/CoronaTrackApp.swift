9d5870e4fc9cf1e0a894839584a8b4d0810cf5ce
kbeliauski <bkotutby1112@icloud.com>
2020-10-11 16:09:26 -0400

    Initial Commit

---
+++ CoronaTrack/CoronaTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoronaTrackApp.swift
+//  CoronaTrack
+//
+//  Created by Kiryl Beliauski on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct CoronaTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
